0e9fa5d379035e7f6681e03d2725da09cda49ca7
Christos Patsouras <32415524+patschris@users.noreply.github.com>
2019-04-09 12:57:05 +0300

    Change login and register

--- src/RESTws/Login.java
+++ src/RESTws/Login.java
@@ -4,14 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
 
 import obj_classes.User;
@@ -19,35 +22,33 @@ import obj_classes.User;
 @Path("/login")
 public class Login {
 	
+	private String query = "select * from users where username=? and password=?";
+	
 	@POST
-	@Produces(MediaType.TEXT_PLAIN)
-	@Consumes(MediaType.TEXT_PLAIN)
-	public String register(String userString) throws SQLException, ClassNotFoundException {
-		
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response register(String input) throws SQLException, ClassNotFoundException {
+		int status;
+		ObjectNode objectNode = new ObjectMapper().createObjectNode();
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/m111","root","root");
-		
-		Gson g = new Gson();
-		User user = g.fromJson(userString, User.class);
-		
-		Statement stmt = con.createStatement();  
-		ResultSet rs = stmt.executeQuery("select * from users");
-		
-		while(rs.next()) {
-			String uname = rs.getString(2);
-			String pass = rs.getString(3);
-			if(uname.equals(user.getName())) {
-				if(pass.equals(user.getPassword())) {
-					con.close();
-					return "Authorized";
-				}
-				
-			}
+		User user = new Gson().fromJson(input, User.class);
+		PreparedStatement stmt = con.prepareStatement(this.query);
+		stmt.setString(1, user.getName());
+		stmt.setString(2, user.getPassword());
+		ResultSet rs = stmt.executeQuery();
+		if (rs.next()) {
+			status = 200;
+			objectNode.put("status", "Ok");
 		}
-				
+		else {
+			status = 400;
+			objectNode.put("status", "Failed");
+		}
+		rs.close();
+		stmt.close();
 		con.close();  
-		
-		return "Not Autorized";
-
+		return Response.status(status).entity(objectNode.toString()).build();
 	}
+
 }
--- src/RESTws/Register.java
+++ src/RESTws/Register.java
@@ -1,45 +1,70 @@
 package RESTws;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import com.google.gson.Gson;
-import java.sql.*;
+import javax.ws.rs.core.Response;
 
-import obj_classes.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.gson.Gson;
+
+import obj_classes.User;
 
 
 @Path("/register")
 public class Register {	
+	
+	private String query = "select * from users where username=?";
+	private String updateStatement = "insert into users (username, password) values (?,?)";
 
+	
 	@POST
-	@Produces(MediaType.TEXT_PLAIN)
-	@Consumes(MediaType.TEXT_PLAIN)
-	public String register(String userString) throws SQLException, ClassNotFoundException {
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response register(String input) throws SQLException, ClassNotFoundException {
+		int status;
+		ObjectNode objectNode = new ObjectMapper().createObjectNode();
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/m111","root","root");
-		
-		Gson g = new Gson();
-		User user = g.fromJson(userString, User.class);
-		
-		Statement stmt = con.createStatement();  
-		ResultSet rs = stmt.executeQuery("select * from users");
-		
-		while(rs.next()) {
-			String uname = rs.getString(2);
-			if(uname.equals(user.getName())) {
-				con.close();
-				return "This username already exists!";
+		User user = new Gson().fromJson(input, User.class);
+		PreparedStatement stmt = con.prepareStatement(this.query);
+		stmt.setString(1, user.getName());
+		ResultSet rs = stmt.executeQuery();
+		int count = 0;
+		while (rs.next()) count++;
+		rs.close();
+		stmt.close();
+		if (count == 0) {
+			stmt = con.prepareStatement(this.updateStatement);
+			stmt.setString(1, user.getName());
+			stmt.setString(2, user.getPassword());
+			int rowsAffected = stmt.executeUpdate();
+			stmt.close();
+			con.close();
+			if (rowsAffected > 0) {
+				status = 200;
+				objectNode.put("status", "Ok");
+			}
+			else {
+				status = 500;
+				objectNode.put("status", "Failed");
 			}
 		}
-		
-		stmt.executeUpdate("insert into users (username ,password) values (" + "'" + user.getName() + "'" +  "," +  "'" + user.getPassword() + "'" +")");
-				
-		con.close();  
-		
-		return "Success!! New account created with username: " + user.getName();
-
+		else {
+			status = 400;
+			objectNode.put("status", "Already exists");
+		}
+		return Response.status(status).entity(objectNode.toString()).build();
 	}
 }
